adcf39a6d39fe606c34c070c5fa0969466704981
NiverMtz <nivermartinezmed@gmail.com>
2021-06-06 18:38:40 -0500

    Upload files for my page

---
+++ javascript/examples.js
@@ -0,0 +1,16 @@
+//DOM
+// let links = document.querySelectorAll("a");
+
+// links.forEach(function(link){
+//     console.log(link)
+// })
+
+
+//OBTENER LAS CELDAS Y RECORRELAS
+// let celdas = document.querySelectorAll("td");
+
+// celdas.forEach(function(td){
+//     td.addEventListener('click', function(){
+//         console.log(this);
+//     })
+// })
---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+// Obtener los elementos de una clase (rt-list)
+let boton = document.querySelectorAll('.rt-list');
+let datos;
+
+// Recorrerlos
+boton.forEach(function(l){
+    l.addEventListener('click', function(ev){
+        // Agregar un evento click a cada uno de ellos
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+        content.classList.remove('animate__animated');
+        content.classList.remove('animate__zoomInDown');
+
+        content.classList.add('animate__animated');
+        content.classList.add('animate__slideOutUp');
+
+        setTimeout(function(){
+            location.href = "/";
+        }, 1100);
+
+        return false;
+    });
+});
